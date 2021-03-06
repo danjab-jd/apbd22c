45c3ccaf645de21c874bd406489a796a25758f5a
Daniel Jabłoński <daniel.jablonski@itsol.tech>
2021-03-06 12:35:44 +0100

    CSharp basics

---
+++ FirstTutorial/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FirstTutorial
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // AddStudent() - PascalCase
+            // addStudent() - camelCase
+
+            Console.WriteLine("Hello World!");
+
+            var stud = new Student
+            {
+                FirstName = "Piotr",
+                Age = 23
+            };
+
+            // Interpolacje stringa
+            Console.WriteLine($"Imie studenta to: {stud.FirstName} i ma {stud.Age} lat");
+
+            var tmp = "ABC";
+            tmp = "CBA";
+
+
+            var list = new List<string> {"Ala", "ma", "kota"};
+
+
+            foreach (var wrt in list)
+            {
+                Console.WriteLine(wrt);
+            }
+
+
+        }
+    }
+}
---
+++ FirstTutorial/Student.cs
@@ -0,0 +1,12 @@
+﻿
+namespace FirstTutorial
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+
+        public int Age { get; set; }
+
+
+    }
+}
